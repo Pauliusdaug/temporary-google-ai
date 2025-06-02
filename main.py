cd7a8d5c10abf427cef9fa3aa7d98e8dfeb6d964
Pauliusdaug <pauliusdaugilis@gmail.com>
2025-06-02 14:51:24 +0300

    Cookie recipe generation code added

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def main():
-    print("Hello from temporary-google-ai!")
+import os
+from google import genai
+from dotenv import load_dotenv
+from rich import print
+from pydantic import BaseModel
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
+
+
+
+
+
+class Recipe(BaseModel):
+    recipe_name: str
+    ingredients: list[str]
+
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="List a fe popular cookie recipe, and include the amounts of ingredients.",
+    config={
+        "response_mime_type": "application/json",
+        "response_schema": Recipe,
+    },
+)
+# Use the response as a JSON string.
+print(response.text)
+
+# Use instantiated objects.
+recipe: Recipe = response.parsed
+
+print(recipe)
